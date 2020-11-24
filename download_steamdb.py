57da5e170df6d906ff7eba9074d88eae28e5d0cf
Wok <woctezuma@users.noreply.github.com>
2020-11-24 10:51:55 +0100

    Download and parse follow data from SteamDB

---
+++ download_steamdb.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import requests
+
+from utils import get_steamdb_filename
+
+
+def get_steamdb_url():
+    return 'https://steamdb.info/upcoming/?hype'
+
+
+def get_headers():
+    # To avoid status code 403 ("Forbidden"):
+    return {'user-agent': 'my-app/0.0.1'}
+
+
+def download_steamdb():
+    text_aggregate = ''
+    r = requests.get(url=get_steamdb_url(),
+                     headers=get_headers())
+    if r.ok:
+        text_aggregate = r.text
+    return text_aggregate
+
+
+def save_steamdb_to_disk():
+    if not Path(get_steamdb_filename()).exists():
+        text_aggregate = download_steamdb()
+        if len(text_aggregate) > 0:
+            with open(get_steamdb_filename(), 'w', encoding='utf8') as f:
+                f.write(text_aggregate)
+    return True
+
+
+def main():
+    save_steamdb_to_disk()
+
+
+if __name__ == '__main__':
+    main()
---
+++ parse_steamdb.py
@@ -0,0 +1,26 @@
+from utils import get_steamdb_filename
+
+
+def filter_steamdb_document(lines):
+    return [l for l in lines if l.startswith('<a href="/app/')]
+
+
+def parse_steamdb_app_id(line):
+    return int(line.split('/')[2])
+
+
+def load_steamdb_ranking():
+    with open(get_steamdb_filename(), 'r', encoding='utf8') as f:
+        d = f.readlines()
+    steamdb_ranking = [parse_steamdb_app_id(l) for l in filter_steamdb_document(d)]
+    print('[SteamDB] #apps = {}'.format(len(steamdb_ranking)))
+    return steamdb_ranking
+
+
+def main():
+    steamdb_ranking = load_steamdb_ranking()
+    return True
+
+
+if __name__ == '__main__':
+    main()
